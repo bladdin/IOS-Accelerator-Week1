8bcf6ad15cab7a225cba44b79684191dd3755a31
Benjamin Laddin <benjamin.laddin@gmail.com>
2015-08-06 11:40:40 -0700

    day 3 hw upload 2

---
+++ TwitterWeek1/DetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DetailViewController.swift
+//  TwitterWeek1
+//
+//  Created by Benjamin Laddin on 8/6/15.
+//  Copyright (c) 2015 Benjamin Laddin. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+  
+  @IBOutlet weak var dvcLabel: UILabel!
+  
+  var tweets = [Tweet]()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    
+  }
+  
+}
---
+++ TwitterWeek1/TweetCell.swift
@@ -0,0 +1,16 @@
+//
+//  TweetCell.swift
+//  TwitterWeek1
+//
+//  Created by Benjamin Laddin on 8/6/15.
+//  Copyright (c) 2015 Benjamin Laddin. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+  
+  @IBOutlet weak var usernameLabel: UILabel!
+  
+  @IBOutlet weak var tweetTextLabel: UILabel!
+}
